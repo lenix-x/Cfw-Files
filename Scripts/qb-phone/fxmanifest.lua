3496a624c3fd3cb8ae43300f636eb6591d993f8d
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-12-24 03:51:38 +0300

    Add files via upload

---
+++ Scripts/qb-phone/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'bodacious'
+game 'gta5'
+name 'https://github.com/lenix-x'
+
+author 'Lenix__x'
+
+description 'qb-phone'
+version '1.3.0'
+
+ui_page 'html/index.html'
+
+shared_scripts {
+    'config.lua',
+    '@qb-apartments/config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/animation.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+files {
+    'html/*.html',
+    'html/js/*.js',
+    'html/img/*.png',
+    'html/css/*.css',
+    'html/img/backgrounds/*.png',
+    'html/img/apps/*.png',
+}
+
+lua54 'yes'
