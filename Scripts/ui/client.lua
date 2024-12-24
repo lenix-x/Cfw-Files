d0ea427413103ecf0be5d5e9e3bc9036bfa40943
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-12-24 14:45:42 +0300

    Add files via upload

---
+++ Scripts/ui/client.lua
@@ -0,0 +1,39 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+function Show(title, content)
+    SendNUIMessage({
+        action = "open",
+        title = title,
+        content = content
+    })
+end
+
+function Close()
+    
+    SendNUIMessage({
+        action = "close",
+    })
+end
+
+exports("Show", Show)
+exports("Close", Close)
+
+
+RegisterCommand('open', function(source, args, rawCommand)
+    if #args < 2 then
+        Show(args[1])
+        return
+    end
+
+    print(args[1])
+    print("[FiveM Store] Opening UI with title: " .. args[1] .. ", content: " .. args[2])
+
+    -- Assuming Show is a function defined elsewhere in your script
+    Show(args[1], args[2])
+end)
+
+
+RegisterCommand('close', function(source, args, RawCommand)
+    
+    Close()
+end)
